df5acbc1a87b3437b469a10da8c77b5de4c17887
YhinkaDevOps <yinkabakare30@gnail.com>
2023-02-20 03:04:47 +0200

    add components to Browser router

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ function App() {
   return (
     <div className="overflow-hidden">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
+        <Sidebar />
+        <Footer />
       </Router>
     </div>
   );
