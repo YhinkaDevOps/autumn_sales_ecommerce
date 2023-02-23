0497496cca4441e2603bd1a6527ed3c962636f33
YhinkaDevOps <yinkabakare30@gnail.com>
2023-02-23 23:51:49 +0200

    add sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+export const SidebarContext = createContext();
+
+const SidebarProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <SidebarContext.Provider value={(isOpen, setIsOpen, handleClose)}>
+      {children}
+    </SidebarContext.Provider>
+  );
 };
 
-export default Sidebar;
+export default SidebarProvider;
