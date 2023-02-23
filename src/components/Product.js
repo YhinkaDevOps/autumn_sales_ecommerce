fa0f101d2e0c07329367e384e0b2922f0e7e7ec3
YhinkaDevOps <yinkabakare30@gnail.com>
2023-02-23 23:41:13 +0200

    add home products

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,16 +16,22 @@ const Product = ({ product }) => {
               alt=""
             />
           </div>
-          <div className="absolute top-0 right-0 bg-red-500/40 p-2 flex-col items-center gap-y-2 opacity-0 group-hover:
-          opacity-100">
+          <div className="absolute top-6 right-11 group-hover:right-5 p-2 flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <button>
               <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500"><BsPlus className="text-3xl" /></div>
             </button>
-            <Link to={'/'} className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"><BsEyeFill /></Link>
+            <Link to={`/product/${id}`} className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"><BsEyeFill /></Link>
           </div>
         </div>
       </div>
-      <div></div>
+      <div>
+
+        <div className="text-sm capitalize text-gray-500 mb-1">{category}</div>
+        <Link to={`/product/${id}`}>
+         <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+        <div className="font-semibold">$ {price}</div>
+      </div>
     </div>
   );
 };
